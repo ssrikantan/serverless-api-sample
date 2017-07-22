403e4b66ddfaf25f1af48bcbeb173b9c736206aa
ssrikantan <s_srikantan@hotmail.com>
2017-07-22 22:10:00 +0530

    Add files via upload

---
+++ CreateExpense.cs
@@ -0,0 +1,30 @@
+﻿#r "Newtonsoft.Json"
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+public static async Task<HttpResponseMessage> Run(HttpRequestMessage req,
+                            IAsyncCollector<dynamic> collector, TraceWriter log)
+{
+    log.Info("Received Expense creation request... ");
+    dynamic data = await req.Content.ReadAsAsync<object>();
+    // Create the Expense object dynamically by parsing the Request Body 
+    dynamic expenseDocument = new
+    {
+        id = System.Guid.NewGuid(),
+        empid = data.empid,
+        email = data.email,
+        expensedate = data.expensedate,
+        description = "expenses to meet- " + data.item,
+        amount = data.amount,
+        currency = data.currency,
+        country = data.country,
+        item = data.item
+    };
+    log.Info($"C# expense data inserted: " + expenseDocument);
+    //since this script requires multiple output parameters, an IAsyncCollector<T> is used
+    // this collector stores the expense document, and uses the Output binding to
+    // call Cosmos DB to insert it asynchronously, even as the HttpResponse is returned
+    await collector.AddAsync(expenseDocument);
+    return req.CreateResponse(HttpStatusCode.OK, "expense data inserted");
+}
---
+++ GetAllProjects.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log,
+                                                        IEnumerable<dynamic> documents)
+{
+    log.Info("C# HTTP trigger function processed a request.");
+    return documents == null
+        ? req.CreateResponse(HttpStatusCode.BadRequest, "No data found matching the criteria!")
+        : req.CreateResponse(HttpStatusCode.OK, documents);
+}
